f4f9cf50770f5dac4088645fb7d6731752a18f90
Małgorzata Dziubich <malgorzata.dziubich@gmail.com>
2024-11-04 19:58:35 +0100

    Prepare base for app main view

--- Weather/Features/WeaterSearch/View/CitiesListView.swift
+++ Weather/Features/WeaterSearch/View/CitiesListView.swift
@@ -1,46 +1,59 @@
 //
-//  ContentView.swift
+//  CitiesListView.swift
 //  Weather
 //
 //  Created by Małgorzata Dziubich on 03/11/2024.
 //
 
+import Foundation
 import SwiftUI
-import CoreData
 
-struct ContentView: View {
+struct CitiesListView: View {
 
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
-    
+
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        List {
+            ForEach(items) { item in
+                NavigationLink {
+                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                } label: {
+                    Text(item.timestamp!, formatter: itemFormatter)
+                        .font(.title3)
                 }
-                .onDelete(perform: deleteItems)
+                .listRowBackground(Color.green)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+            .onDelete(perform: deleteItems)
+            .background(Color.red)
+        }
+        .scrollContentBackground(.hidden)
+    }
+
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
-            Text("Select an item")
         }
     }
 
@@ -59,30 +72,8 @@ struct ContentView: View {
             }
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    CitiesListView()
 }
---
+++ Weather/Features/WeaterSearch/View/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Małgorzata Dziubich on 03/11/2024.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    @State private var searchInput: String = ""
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                MapView()
+                    .frame(height: UIScreen.main.bounds.height / 4)
+
+                TextField("Search city", text: $searchInput)
+                    .frame(height: 20.0)
+                    .padding(16)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .offset(y: -30)
+                    .padding(.bottom, -46)
+
+                CitiesListView()
+                Spacer()
+            }
+            .background(Color.purple)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+//                    ToolbarItem {
+//                        Button(action: addItem) {
+//                            Label("Add Item", systemImage: "plus")
+//                        }
+//                    }
+            }
+            .navigationBarTitle("Weather")
+        }
+    }
+}
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
